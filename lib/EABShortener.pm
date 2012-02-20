e519bfd9e26da258ccd66d7621a52ba96402223e
Alan Berndt <alan@eatabrick.org>
2012-02-20 09:46:03 -0700

    Use SQLite storage engine

--- lib/EABShortener.pm
+++ lib/EABShortener.pm
@@ -1,13 +1,11 @@
 package EABShortener;
 use Dancer ':syntax';
+use Dancer::Plugin::Database;
 
 our $VERSION = '0.1';
 
 use URI::Title 'title';
 use Data::Validate::URI 'is_uri';
-
-my @links = ();
-my %links = ();
 
 get '/' => sub {
   template 'index';
@@ -17,20 +15,14 @@ my @chars = split //, 'abcdefghijklmnopqrstuvwxyz0123456789';
 post '/' => sub {
   if (is_uri(params->{uri})) {
     my $token = join '', map $chars[int(rand(@chars))], 1 .. 6;
-    my $link = {
-      token => $token,
-      uri   => params->{uri},
-      title => title(params->{uri}) || 'Unknown link',
-      user  => params->{user} || 'Some asshole',
-    };
 
-    unshift @links, $link;
-    $links{$token} = params->{uri};
-
-    if (@links > config->{max_links}) {
-      my $old = pop @links;
-      delete $links{$old->{token}};
-    }
+    database->quick_insert(links => {
+      token   => $token,
+      uri     => params->{uri},
+      title   => title(params->{uri}) || 'Unknown link',
+      user    => params->{user} || 'Some asshole',
+      created => time,
+    });
 
     content_type 'application/json';
     return to_json { result => "http://eab.so/$token" };
@@ -42,6 +34,11 @@ post '/' => sub {
 
 get '/rss' => sub {
   content_type 'text/xml';
+
+  my @links = database->quick_select(links => {}, { 
+    order_by => { desc => 'created' }, 
+    limit => 25,
+  });
   template 'rss', { links => \@links };
 };
 
@@ -55,8 +52,10 @@ any '/new.pl' => sub {
 };
 
 get '/:token' => sub {
-  if (exists $links{params->{token}}) {
-    redirect $links{params->{token}};
+  my $link = database->quick_select(links => { token => params->{token} });
+
+  if ($link) {
+    redirect $link->{uri};
   } else {
     status 'not_found';
     template '404';
---
+++ schema.sql
@@ -0,0 +1,11 @@
+create table if not exists links (
+  token   text    not null primary key,
+  uri     text    not null,
+  title   text    not null,
+  user    text    not null,
+  created integer not null
+);
+
+create index if not exists created_desc on links (
+  created desc
+);
